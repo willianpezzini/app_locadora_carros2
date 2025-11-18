ccb6136b955a1f4367b658aa35de736213c7e18b
Willian Hercilio Pezzini <williantijucas@gmail.com>
2025-11-18 06:38:05 -0300

    Adicionado o token de autorização aop cabeçalho da requisição

--- public/js/app.js
+++ public/js/app.js
@@ -2297,8 +2297,13 @@ __webpack_require__.r(__webpack_exports__);
 /*!*************************************************************************************************************************************************************************************************************!*\
   !*** ./node_modules/babel-loader/lib/index.js??clonedRuleSet-5[0].rules[0].use[0]!./node_modules/vue-loader/lib/index.js??vue-loader-options!./resources/js/components/Marcas.vue?vue&type=script&lang=js& ***!
   \*************************************************************************************************************************************************************************************************************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
 //
 //
 //
@@ -2377,6 +2382,50 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
+  computed: {
+    token: function token() {
+      var token = document.cookie.split(';').find(function (indice) {
+        return indice.startsWith('token=');
+      });
+      token = token.split('=')[1];
+      token = 'Bearer ' + token;
+      console.log(token);
+      return token;
+    }
+  },
+  data: function data() {
+    return {
+      urlBase: 'http://localhost:8000/api/v1/marca',
+      nomeMarca: '',
+      arquivoImagem: []
+    };
+  },
+  methods: {
+    carregarImagem: function carregarImagem(e) {
+      this.arquivoImagem = e.target.files;
+    },
+    salvar: function salvar() {
+      console.log(this.nomeMarca, this.arquivoImagem);
+      var formData = new FormData();
+      formData.append('nome', this.nomeMarca);
+      formData.append('imagem', this.arquivoImagem[0]);
+      var config = {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+          'Accept': 'application/json',
+          'Authorization': this.token
+        }
+      };
+      axios.post(this.urlBase, formData, config).then(function (response) {
+        console.log(response);
+      })["catch"](function (error) {
+        console.log(error);
+      });
+    }
+  }
+});
 
 /***/ }),
 
@@ -37989,69 +38038,67 @@ var render = function () {
                   key: "body",
                   fn: function () {
                     return [
-                      _c("div", [
-                        _c(
-                          "div",
-                          { staticClass: "col mb-3" },
-                          [
-                            _c(
-                              "input-container-component",
-                              {
-                                attrs: {
-                                  titulo: "ID",
-                                  id: "InputId",
-                                  "id-help": "idHelp",
-                                  "texto-ajuda":
-                                    "Opcional. Informe o Id da marca",
-                                },
+                      _c(
+                        "div",
+                        { staticClass: "col mb-3" },
+                        [
+                          _c(
+                            "input-container-component",
+                            {
+                              attrs: {
+                                titulo: "ID",
+                                id: "InputId",
+                                "id-help": "idHelp",
+                                "texto-ajuda":
+                                  "Opcional. Informe o Id da marca",
                               },
-                              [
-                                _c("input", {
-                                  staticClass: "form-control",
-                                  attrs: {
-                                    type: "number",
-                                    id: "inputId",
-                                    "aria-describedby": "idHelp",
-                                    placeholder: "Id",
-                                  },
-                                }),
-                              ]
-                            ),
-                          ],
-                          1
-                        ),
-                        _vm._v(" "),
-                        _c(
-                          "div",
-                          { staticClass: "mb-3" },
-                          [
-                            _c(
-                              "input-container-component",
-                              {
+                            },
+                            [
+                              _c("input", {
+                                staticClass: "form-control",
                                 attrs: {
-                                  titulo: "Nome da Marca",
-                                  id: "InputNome",
-                                  "id-help": "nomeHelp",
-                                  "texto-ajuda":
-                                    "Opcional. Informe o Nome da marca",
+                                  type: "number",
+                                  id: "inputId",
+                                  "aria-describedby": "idHelp",
+                                  placeholder: "Id",
                                 },
+                              }),
+                            ]
+                          ),
+                        ],
+                        1
+                      ),
+                      _vm._v(" "),
+                      _c(
+                        "div",
+                        { staticClass: "col mb-3" },
+                        [
+                          _c(
+                            "input-container-component",
+                            {
+                              attrs: {
+                                titulo: "Nome da Marca",
+                                id: "InputNome",
+                                "id-help": "nomeHelp",
+                                "texto-ajuda":
+                                  "Opcional. Informe o Nome da marca",
                               },
-                              [
-                                _c("input", {
-                                  staticClass: "form-control",
-                                  attrs: {
-                                    type: "text",
-                                    id: "inputNome",
-                                    "aria-describedby": "nomeHelp",
-                                    placeholder: "Nome da Marca",
-                                  },
-                                }),
-                              ]
-                            ),
-                          ],
-                          1
-                        ),
-                      ]),
+                            },
+                            [
+                              _c("input", {
+                                staticClass: "form-control",
+                                attrs: {
+                                  type: "text",
+                                  id: "inputNome",
+                                  "aria-describedby": "nomeHelp",
+                                  placeholder: "Nome da Marca",
+                                },
+                              }),
+                            ]
+                          ),
+                        ],
+                        1
+                      ),
                     ]
                   },
                   proxy: true,
@@ -38135,6 +38182,14 @@ var render = function () {
                       },
                       [
                         _c("input", {
+                          directives: [
+                            {
+                              name: "model",
+                              rawName: "v-model",
+                              value: _vm.nomeMarca,
+                              expression: "nomeMarca",
+                            },
+                          ],
                           staticClass: "form-control",
                           attrs: {
                             type: "text",
@@ -38142,8 +38197,22 @@ var render = function () {
                             "aria-describedby": "novoNomeHelp",
                             placeholder: "Nome da Marca",
                           },
+                          domProps: { value: _vm.nomeMarca },
+                          on: {
+                            input: function ($event) {
+                              if ($event.target.composing) {
+                                return
+                              }
+                              _vm.nomeMarca = $event.target.value
+                            },
+                          },
                         }),
                       ]
+                    ),
+                    _vm._v(
+                      "\n                " +
+                        _vm._s(_vm.nomeMarca) +
+                        "\n            "
                     ),
                   ],
                   1
@@ -38172,8 +38241,18 @@ var render = function () {
                             "aria-describedby": "novaImagemHelp",
                             placeholder: "Selecione uma imagem",
                           },
+                          on: {
+                            change: function ($event) {
+                              return _vm.carregarImagem($event)
+                            },
+                          },
                         }),
                       ]
+                    ),
+                    _vm._v(
+                      "\n                " +
+                        _vm._s(_vm.arquivoImagem) +
+                        "\n            "
                     ),
                   ],
                   1
@@ -38197,7 +38276,11 @@ var render = function () {
                 _vm._v(" "),
                 _c(
                   "button",
-                  { staticClass: "btn btn-primary", attrs: { type: "button" } },
+                  {
+                    staticClass: "btn btn-primary",
+                    attrs: { type: "button" },
+                    on: { click: _vm.salvar },
+                  },
                   [_vm._v("Salvar")]
                 ),
               ]
@@ -50937,9 +51020,6 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ });
 /* harmony import */ var _Marcas_vue_vue_type_template_id_0f047062___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./Marcas.vue?vue&type=template&id=0f047062& */ "./resources/js/components/Marcas.vue?vue&type=template&id=0f047062&");
 /* harmony import */ var _Marcas_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./Marcas.vue?vue&type=script&lang=js& */ "./resources/js/components/Marcas.vue?vue&type=script&lang=js&");
-/* harmony reexport (unknown) */ var __WEBPACK_REEXPORT_OBJECT__ = {};
-/* harmony reexport (unknown) */ for(const __WEBPACK_IMPORT_KEY__ in _Marcas_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__) if(__WEBPACK_IMPORT_KEY__ !== "default") __WEBPACK_REEXPORT_OBJECT__[__WEBPACK_IMPORT_KEY__] = () => _Marcas_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__[__WEBPACK_IMPORT_KEY__]
-/* harmony reexport (unknown) */ __webpack_require__.d(__webpack_exports__, __WEBPACK_REEXPORT_OBJECT__);
 /* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! !../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
 
 
@@ -50979,11 +51059,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
 /* harmony import */ var _node_modules_babel_loader_lib_index_js_clonedRuleSet_5_0_rules_0_use_0_node_modules_vue_loader_lib_index_js_vue_loader_options_Marcas_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../node_modules/babel-loader/lib/index.js??clonedRuleSet-5[0].rules[0].use[0]!../../../node_modules/vue-loader/lib/index.js??vue-loader-options!./Marcas.vue?vue&type=script&lang=js& */ "./node_modules/babel-loader/lib/index.js??clonedRuleSet-5[0].rules[0].use[0]!./node_modules/vue-loader/lib/index.js??vue-loader-options!./resources/js/components/Marcas.vue?vue&type=script&lang=js&");
-/* harmony import */ var _node_modules_babel_loader_lib_index_js_clonedRuleSet_5_0_rules_0_use_0_node_modules_vue_loader_lib_index_js_vue_loader_options_Marcas_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_babel_loader_lib_index_js_clonedRuleSet_5_0_rules_0_use_0_node_modules_vue_loader_lib_index_js_vue_loader_options_Marcas_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__);
-/* harmony reexport (unknown) */ var __WEBPACK_REEXPORT_OBJECT__ = {};
-/* harmony reexport (unknown) */ for(const __WEBPACK_IMPORT_KEY__ in _node_modules_babel_loader_lib_index_js_clonedRuleSet_5_0_rules_0_use_0_node_modules_vue_loader_lib_index_js_vue_loader_options_Marcas_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__) if(__WEBPACK_IMPORT_KEY__ !== "default") __WEBPACK_REEXPORT_OBJECT__[__WEBPACK_IMPORT_KEY__] = () => _node_modules_babel_loader_lib_index_js_clonedRuleSet_5_0_rules_0_use_0_node_modules_vue_loader_lib_index_js_vue_loader_options_Marcas_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__[__WEBPACK_IMPORT_KEY__]
-/* harmony reexport (unknown) */ __webpack_require__.d(__webpack_exports__, __WEBPACK_REEXPORT_OBJECT__);
- /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ((_node_modules_babel_loader_lib_index_js_clonedRuleSet_5_0_rules_0_use_0_node_modules_vue_loader_lib_index_js_vue_loader_options_Marcas_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0___default())); 
+ /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (_node_modules_babel_loader_lib_index_js_clonedRuleSet_5_0_rules_0_use_0_node_modules_vue_loader_lib_index_js_vue_loader_options_Marcas_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__["default"]); 
 
 /***/ }),
 
--- resources/js/components/Marcas.vue
+++ resources/js/components/Marcas.vue
@@ -5,25 +5,23 @@
                 <!-- Inicio card de busca -->
                 <card-component title="Busca de Marcas">
                     <template v-slot:body>
-                        <div>
-                            <div class="col mb-3">
-                                <input-container-component titulo="ID" id="InputId" id-help="idHelp"
-                                    texto-ajuda="Opcional. Informe o Id da marca">
-                                    <input type="number" class="form-control" id="inputId" aria-describedby="idHelp"
-                                        placeholder="Id">
+                        <div class="col mb-3">
+                            <input-container-component titulo="ID" id="InputId" id-help="idHelp"
+                                texto-ajuda="Opcional. Informe o Id da marca">
+                                <input type="number" class="form-control" id="inputId" aria-describedby="idHelp"
+                                    placeholder="Id">
 
-                                </input-container-component>
-                            </div>
-
-                            <div class="mb-3">
-                                <input-container-component titulo="Nome da Marca" id="InputNome" id-help="nomeHelp"
-                                    texto-ajuda="Opcional. Informe o Nome da marca">
-                                    <input type="text" class="form-control" id="inputNome" aria-describedby="nomeHelp"
-                                        placeholder="Nome da Marca">
-
-                                </input-container-component>
-                            </div>
+                            </input-container-component>
                         </div>
+                        <div class="col mb-3">
+                            <input-container-component titulo="Nome da Marca" id="InputNome" id-help="nomeHelp"
+                                texto-ajuda="Opcional. Informe o Nome da marca">
+                                <input type="text" class="form-control" id="inputNome" aria-describedby="nomeHelp"
+                                    placeholder="Nome da Marca">
+
+                            </input-container-component>
+                        </div>
+
                     </template>
 
                     <template v-slot:footer>
@@ -53,28 +51,85 @@
                     <input-container-component titulo="Nome da Marca" id="novoNome" id-help="novoNomeHelp"
                         texto-ajuda="Opcional. Informe o Nome da marca">
                         <input type="text" class="form-control" id="novoNome" aria-describedby="novoNomeHelp"
-                            placeholder="Nome da Marca">
+                            placeholder="Nome da Marca" v-model="nomeMarca">
 
                     </input-container-component>
+                    {{ nomeMarca }}
                 </div>
 
                 <div class="form-group">
                     <input-container-component titulo="Imagem" id="novaImagem" id-help="novaImagemHelp"
                         texto-ajuda="Selecione uma imagem no formato PNG.">
                         <input type="file" class="form-control-file" id="novaImagem" aria-describedby="novaImagemHelp"
-                            placeholder="Selecione uma imagem">
+                            placeholder="Selecione uma imagem" @change="carregarImagem($event)">
 
                     </input-container-component>
+                    {{ arquivoImagem }}
                 </div>
             </template>
 
             <template v-slot:rodape>
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
-                <button type="button" class="btn btn-primary">Salvar</button>
+                <button type="button" class="btn btn-primary" @click="salvar">Salvar</button>
             </template>
         </modal-component>
     </div>
 </template>
 
 <script>
+
+export default {
+    computed: {
+        token() {
+            let token = document.cookie.split(';').find(indice => {
+                return indice.startsWith('token=')
+            })
+
+            token = token.split('=')[1]
+            token = 'Bearer ' + token
+
+            console.log(token)
+            return token
+        }
+    },
+
+    data() {
+        return {
+            urlBase: 'http://localhost:8000/api/v1/marca',
+            nomeMarca: '',
+            arquivoImagem: []
+        }
+    },
+    methods: {
+        carregarImagem(e) {
+            this.arquivoImagem = e.target.files
+        },
+        salvar() {
+            console.log(this.nomeMarca, this.arquivoImagem)
+
+            let formData = new FormData();
+            formData.append('nome', this.nomeMarca)
+            formData.append('imagem', this.arquivoImagem[0])
+
+            let config = {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                    'Accept': 'application/json',
+                    'Authorization': this.token
+                }
+            }
+
+
+
+            axios.post(this.urlBase, formData, config)
+                .then(response => {
+                    console.log(response)
+                })
+
+                .catch(error => {
+                    console.log(error)
+                })
+        }
+    }
+}
 </script>
