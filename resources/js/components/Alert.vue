e2826539fa99df17ffe2163e9a87b81b5dbb058f
Willian Hercilio Pezzini <williantijucas@gmail.com>
2025-11-19 07:05:13 -0300

    Inserindo as mensagens de alerta na criação das marcas

---
+++ resources/js/components/Alert.vue
@@ -0,0 +1,18 @@
+<template>
+    <div :class="estilo" role="alert">
+        Mensagem de Sucesso
+        {{ tipo }}
+    </div>
+
+</template>
+
+<script>
+export default {
+    props: ['tipo'],
+    computed: {
+        estilo() {
+            return 'alert alert-'+this.tipo
+        }
+    }
+}
+</script>
--- resources/js/components/Marcas.vue
+++ resources/js/components/Marcas.vue
@@ -46,6 +46,11 @@
             </div>
         </div>
         <modal-component id="modalMarca" titulo="Adicionar Marca">
+
+            <template v-slot:alertas>
+                <alert-component tipo="success"></alert-component>
+                <alert-component tipo="danger"></alert-component>
+            </template>
             <template v-slot:conteudo>
                 <div class="form-group">
                     <input-container-component titulo="Nome da Marca" id="novoNome" id-help="novoNomeHelp"
